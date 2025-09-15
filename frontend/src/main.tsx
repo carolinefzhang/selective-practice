fde20453d7729b4c144696728b462ab2d120a42d
Caroline Zhang <caroline.fzhang@gmail.com>
2025-09-15 20:51:50 +1000

    add file extension to solve amplify build error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { App } from './app'
+import { App } from './app.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Auth0ProviderWithNavigate } from './auth/auth0-provider-with-navigate'
 
